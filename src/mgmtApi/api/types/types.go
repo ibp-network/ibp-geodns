3046ac983520040635783bad07e8aa9c8d0ceba1
Tom <tsenseless@gmail.com>
2025-05-11 11:01:38 -0400

    Update

--- src/mgmtApi/api/types/types.go
+++ src/mgmtApi/api/types/types.go
@@ -83,3 +83,31 @@ type OfficialResults = struct {
 	EndpointResults []mon.EndpointResult
 	Mu              sync.RWMutex
 }
+
+// Response represents a DNS query response.
+type ApiResponse struct {
+	Result interface{} `json:"result"`
+	Error  string      `json:"error"`
+}
+
+// ApiRequest represents an API request structure.
+type ApiRequest struct {
+	Method     string `json:"Method"`
+	Action     string `json:"Action"`
+	Output     string `json:"Output"`
+	MemberName string `json:"MemberName"`
+	Year       string `json:"Year"`
+	Month      string `json:"Month"`
+	StartYear  string `json:"StartYear"`
+	StartMonth string `json:"StartMonth"`
+	EndYear    string `json:"EndYear"`
+	EndMonth   string `json:"EndMonth"`
+	AuthKey    string `json:"Authkey"`
+}
+
+type CheckResult struct {
+	CheckName string                 `json:"checkName"`
+	Status    bool                   `json:"status"`
+	ErrorText string                 `json:"errorText"`
+	Data      map[string]interface{} `json:"data"`
+}
