cdc26e4562a28990536ef8a973f6454e625efb37
Tom <tsenseless@gmail.com>
2025-05-24 11:45:12 -0400

    Update

--- src/dnsApi/dnsApi.go
+++ src/dnsApi/dnsApi.go
@@ -16,7 +16,7 @@ import (
 var version = "0.7.0"
 
 func main() {
-	log.SetLogLevel(log.Info)
+	log.SetLogLevel(log.Debug)
 	log.Log(log.Info, "IBP-GeoDNS DNS backend v%s starting...", version)
 
 	cfgFile := flag.String("config", "config.json", "Path to configuration file")
