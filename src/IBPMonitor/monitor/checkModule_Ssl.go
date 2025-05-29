aca866c15035575af9d57a6f62519f34543aff7a
Tom <tsenseless@gmail.com>
2025-05-29 03:53:41 -0400

    Update

--- src/IBPMonitor/monitor/checkModule_Ssl.go
+++ src/IBPMonitor/monitor/checkModule_Ssl.go
@@ -8,17 +8,19 @@ import (
 	cfg "ibp-geodns/src/common/config"
 )
 
-// SslCheck is registered for "ssl" domain checks (see init() below).
+// We register the SSL check under name "ssl" so it is invoked if
+// the config has a domain-level check with { "Name": "ssl", "CheckType": "domain" }.
 func init() {
 	RegisterDomainCheck("ssl", SslCheck)
 }
 
-// SslCheck runs an SSL check on port 443 for either IPv4 or IPv6 (fallback).
+// SslCheck tries IPv4 first, then IPv6 (if present). It attempts a
+// TLS handshake on port 443, inspects the certificate for upcoming expiry.
 func SslCheck(check cfg.Check, domain string, service cfg.Service, member cfg.Member) {
 	ip4 := member.Service.ServiceIPv4
 	ip6 := member.Service.ServiceIPv6
 
-	// We'll attempt IPv4 first if available, else fallback to IPv6 if present.
+	// If no IP is configured, fail immediately.
 	if ip4 == "" && ip6 == "" {
 		UpdateDomainResultLocal(check, domain, service, member, false, "No IPv4 or IPv6 configured", nil)
 		return
@@ -26,18 +28,19 @@ func SslCheck(check cfg.Check, domain string, service cfg.Service, member cfg.Me
 
 	success := false
 	errText := ""
-	dataMap := make(map[string]interface{})
+	dataMap := map[string]interface{}{}
 
-	// We'll try dialIPv4 first if ip4 is not empty
+	// Attempt IPv4 if present.
 	if ip4 != "" {
 		dialErr := dialAndCheckTLS(check, domain, service, member, ip4, &success, &errText, dataMap)
+		// If success is true (and no dialErr) => we are done.
 		if dialErr == nil && success {
-			return // IPv4 success => done
+			return
 		}
-		// otherwise, we attempt IPv6 if available
+		// Otherwise, we try IPv6 if it exists.
 	}
 
-	// If we haven't succeeded and ip6 is available, do IPv6
+	// If still not success and ip6 is available, try IPv6
 	if !success && ip6 != "" {
 		dialErr := dialAndCheckTLS(check, domain, service, member, ip6, &success, &errText, dataMap)
 		if dialErr == nil && success {
@@ -45,13 +48,22 @@ func SslCheck(check cfg.Check, domain string, service cfg.Service, member cfg.Me
 		}
 	}
 
-	// If still not successful
+	// If we got here => not successful
 	UpdateDomainResultLocal(check, domain, service, member, false, errText, dataMap)
 }
 
-// dialAndCheckTLS attempts to connect to the given ip on port 443, verify TLS, etc.
-func dialAndCheckTLS(check cfg.Check, domain string, service cfg.Service, member cfg.Member, ip string,
-	success *bool, errText *string, dataMap map[string]interface{}) error {
+// dialAndCheckTLS tries a connection to ip:443, verifies the TLS handshake,
+// sets success=false if the cert is nearly expired, or if handshake fails.
+func dialAndCheckTLS(
+	check cfg.Check,
+	domain string,
+	service cfg.Service,
+	member cfg.Member,
+	ip string,
+	success *bool,
+	errText *string,
+	dataMap map[string]interface{},
+) error {
 
 	conn, err := net.DialTimeout("tcp", net.JoinHostPort(ip, "443"),
 		time.Duration(getIntOption(check.ExtraOptions, "ConnectTimeout", 5))*time.Second)
@@ -75,21 +87,26 @@ func dialAndCheckTLS(check cfg.Check, domain string, service cfg.Service, member
 	certs := tlsConn.ConnectionState().PeerCertificates
 	if len(certs) == 0 {
 		*errText = "No cert found"
-		return nil
+		return nil // No error, but success stays false by default
 	}
 
 	cert := certs[0]
 	daysUntilExpiry := int(time.Until(cert.NotAfter).Hours() / 24)
 
+	// Mark success = true for now
 	*success = true
 	*errText = ""
+
+	// If the certificate expires in < 5 days, we consider that "unhealthy".
 	if daysUntilExpiry < 5 {
 		*success = false
 		*errText = "Less than 5 days to expiry"
 	}
+
 	dataMap["ExpiryTimestamp"] = cert.NotAfter.Unix()
 	dataMap["DaysUntilExpiry"] = daysUntilExpiry
 
+	// If still success => we call UpdateDomainResultLocal now for an immediate "true".
 	if *success {
 		UpdateDomainResultLocal(check, domain, service, member, true, "", dataMap)
 	}
