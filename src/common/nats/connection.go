85b1653c8bec85424be7196ff85f8d17619ebc8c
Tom <tsenseless@gmail.com>
2025-06-11 12:04:18 -0400

    Update

--- src/common/nats/connection.go
+++ src/common/nats/connection.go
@@ -200,7 +200,7 @@ func Subscribe(subject string, cb func(*nats.Msg)) (*nats.Subscription, error) {
 	if err != nil {
 		return nil, err
 	}
-	sub.SetPendingLimits(10_000, 10*1024*1024)
+	sub.SetPendingLimits(100000, 32000000)
 	return sub, nil
 }
 
