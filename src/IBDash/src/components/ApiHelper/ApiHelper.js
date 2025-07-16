65873c5c26a71ecd947cca26931ad09161b7b8ad
Tom <tsenseless@gmail.com>
2025-07-16 19:29:11 -0400

    Update

--- src/IBDash/src/components/ApiHelper/ApiHelper.js
+++ src/IBDash/src/components/ApiHelper/ApiHelper.js
@@ -1,13 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || (() => {
-  if (window.location.hostname === 'ibdash.dotters.network') {
-    return window.location.protocol === 'https:' 
-      ? 'https://ibdash.dotters.network:9001/api'
-      : 'http://ibdash.dotters.network:9000/api';
-  }
-  return 'http://localhost:9000/api';
-})();
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://ibdash.dotters.network:9000/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
