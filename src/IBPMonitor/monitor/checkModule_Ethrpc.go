497d4b7bae6e219a3df259af213faffb4834830d
Tom <tsenseless@gmail.com>
2025-07-26 19:19:55 -0400

    Update

--- src/IBPMonitor/monitor/checkModule_Ethrpc.go
+++ src/IBPMonitor/monitor/checkModule_Ethrpc.go
@@ -75,9 +75,9 @@ func runEthrpcSingle(check cfg.Check, endpoint string, service cfg.Service, memb
 
 	// Build URL - convert WSS to HTTPS, WS to HTTP
 	if strings.HasPrefix(u.Protocol, "wss") || strings.HasPrefix(u.Protocol, "https") {
-		reconstructedURL = fmt.Sprintf("https://%s:%s%s/", ip, port, u.Directory)
+		reconstructedURL = fmt.Sprintf("https://%s:%s/%s", ip, port, u.Directory)
 	} else {
-		reconstructedURL = fmt.Sprintf("http://%s:%s%s/", ip, port, u.Directory)
+		reconstructedURL = fmt.Sprintf("http://%s:%s/%s", ip, port, u.Directory)
 	}
 
 	log.Log(log.Debug, "ETHRPC check: original=%s reconstructed=%s for %s", endpoint, reconstructedURL, member.Details.Name)
